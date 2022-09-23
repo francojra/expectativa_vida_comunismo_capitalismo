1d312f267bf6f11166b2811212815defa5fcd8dc
Jeanne Franco <44760301+francojra@users.noreply.github.com>
2022-09-22 23:23:22 -0300

    Add files via upload

---
+++ expectativa_vida_socialismo_capitalismo.R
@@ -0,0 +1,13 @@
+
+# Expectativa de vida em países capitalistas e socialistas --------------------------------------------------------------------------------------
+# Autoria do script: Jeanne Franco --------------------------------------------------------------------------------------------------------------
+# Data: 22/09/22 --------------------------------------------------------------------------------------------------------------------------------
+# Referência: https://ourworldindata.org/life-expectancy ----------------------------------------------------------------------------------------
+
+# Sobre os dados ---------------------------------------------------------------------------------------------------------------------------
+
+### Expectativa de vida é uma métrica para avaliar a saúde da população.
+### É um conceito mais abrangente que a métrica de mortalidade infantil,
+### que foca apenas na mortalidade em idades jovens. A expectativa de vida
+### captura a mortalidade ao longo de todo o curso da vida das pessoas.
+### Isso nos diz a idade média de morte na população.

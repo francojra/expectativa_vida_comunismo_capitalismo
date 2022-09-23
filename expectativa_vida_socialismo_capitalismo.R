ecc2fee30c455364767f4b2483d200489fec92a7
Jeanne Franco <44760301+francojra@users.noreply.github.com>
2022-09-22 23:29:17 -0300

    Add files via upload

--- expectativa_vida_socialismo_capitalismo.R
+++ expectativa_vida_socialismo_capitalismo.R
@@ -11,3 +11,21 @@
 ### que foca apenas na mortalidade em idades jovens. A expectativa de vida
 ### captura a mortalidade ao longo de todo o curso da vida das pessoas.
 ### Isso nos diz a idade média de morte na população.
+
+# Carregar pacotes ------------------------------------------------------------------------------------------------------------------------------
+
+library(tidyverse)
+
+# Carregar dados --------------------------------------------------------------------------------------------------------------------------------
+
+ev <- read.csv("life-expectancy.csv")
+view(ev)
+
+# Manipular dados --------------------------------------------------------------------------------------------------------------------------
+
+ev <- ev %>%
+  select(-Code) %>%
+  view()
+
+ev1 <- ev %>%
+  filter(Entity %in% c(""))
